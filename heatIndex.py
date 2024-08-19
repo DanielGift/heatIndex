88f45660741306ac5d24a374bb9604b8f18fb5ae
Daniel Gift <daniel.gift@sbs.org>
2024-08-19 15:07:45 -0400

    Create heatIndex.py

---
+++ heatIndex.py
@@ -0,0 +1,24 @@
+#*** Start by importing functions from the math library.
+ 
+
+#*** Prompt user to input temperature, store it in a variable & print it
+
+
+#*** Prompt the user for the relative humidity, store it in a variable & print it
+
+
+#*** Define a variable for each of the constants of the formula
+#*** use the first of the two HI formulas - i.e. the one with "only" 9 constants
+#*** found here: http://en.wikipedia.org/wiki/Heat_index#Formula
+
+
+
+#*** Compute the heat index corresponding to the temperature and humidity
+#*** Split it up into three expressions to make it more readable
+
+
+#*** Print the calculated heat index to 3 decimal places
+
+
+#*** Print the rounded up heat index as an integer
+#*** use the ceil function in the math library to round up
